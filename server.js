bbabdbabeca1a090a7e292c84f954ba1fac08134
Yashwarhade8010 <warhadeyash31@gmail.com>
2026-06-20 17:52:00 +0530

    changes

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const PORT = 8080
 const app = express()
 
 app.get("/",(req,res)=>{
-    return res.status(200).json("Hello this is message from server")
+    return res
+      .status(200)
+      .json("Hello this is message from server updated version");
 })
 
 app.listen(PORT,console.log("Server is running on PORT:",PORT))
