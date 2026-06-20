12e80561e21dc5063cd8414c2275a3750d9cf6df
Yashwarhade8010 <warhadeyash31@gmail.com>
2026-06-20 16:10:56 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const PORT = 8080
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    return res.status(200).json("Hello this is message from server")
+})
+
+app.listen(PORT,console.log("Server is running on PORT:",PORT))
